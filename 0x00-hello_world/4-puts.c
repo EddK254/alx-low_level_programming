44a2c514b4e4e30dc78ee44e242f3457d6e859ee
EddK254 <ekairu55@gmail.com>
2023-02-16 06:35:19 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * - main function
+ *
+ *   Return: 0 always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
