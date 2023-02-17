abdefc40a527d0a061b3e565e4e455c621f09daf
EddK254 <ekairu55@gmail.com>
2023-02-17 11:43:31 -0800

    Updated Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Write a program that prints all possible combinations of two two-digit numbers
+ * Description: prints all possible combinations of two two-digit numbers
  *
  * Return : Always 0 (success)
  */
